19dcf88e07ce759ebd4a5b9c691e8066ad6e0b1b
kashifmalik962 <kashifmalik962@gmail.com>
2024-07-02 10:34:42 +0530

    10th commit

--- PyAutoGui_Pynput/canva.py
+++ PyAutoGui_Pynput/canva.py
@@ -1,13 +1,15 @@
 import sys
 import ast
 import time
+import os
+import keyboard
 from threading import Thread
-from PyQt5.QtCore import QUrl, pyqtSlot, QObject, Qt, QTimer
+from PyQt5.QtCore import QUrl, pyqtSlot, QObject, Qt, QCoreApplication
 from PyQt5.QtWidgets import QMainWindow, QApplication, QToolBar, QFrame, QSplitter, QVBoxLayout, QTextEdit, QTabWidget, QToolButton, QLineEdit, QAction, QStatusBar
 from PyQt5.QtWebEngineWidgets import QWebEngineView, QWebEnginePage
 from PyQt5.QtWebChannel import QWebChannel
 from PyQt5.QtWebEngineCore import QWebEngineUrlRequestInterceptor
-from pynput import keyboard, mouse
+from pynput import keyboard as pynput_keyboard, mouse
 import pyautogui
 
 # Declare global variables
@@ -15,6 +17,8 @@ recording = False
 stop_flag = False
 scroll_position = 0
 record_val = False
+pivot = 0
+play_flag = False
 
 # Mapping for special keys
 key_mapping = {
@@ -39,16 +43,24 @@ class CustomWebEnginePage(QWebEnginePage):
         return ""
 
 class WebChannelHandler(QObject):
+    def __init__(self, main_window):
+        super().__init__()
+        self.main_window = main_window  # Store a reference to the main window
+
     @pyqtSlot(int)
     def scrollEvent(self, delta):
-        global scroll_position
-        scroll_position += delta
+        # Use the main window reference to access the browser
+        current_url = self.main_window.browser.url().toString()
+        if current_url not in self.main_window.url_scroll_positions:
+            self.main_window.url_scroll_positions[current_url] = 0
+        self.main_window.url_scroll_positions[current_url] += delta
+
         direction = 'scrolldown' if delta > 0 else 'scrollup'
-        print(f"Scroll event received in PyQt5: {direction}, {abs(delta)}, 0")
         if recording:
             with open('keylogging.txt', 'a') as file:
-                file.write(f"['{direction}', {abs(delta)*6}, 0],\n")
-            print(f"Scroll event recorded: {direction}, {abs(delta)*6}, 0")
+                if direction and delta:
+                    file.write(f"['{direction}', {abs(delta)*2}, 0],\n")
+                print(f"Scroll event recorded: {direction}, {abs(delta)*2}, 0")
 
 class WebEnginePage(QWebEnginePage):
     def __init__(self, main_window, parent=None):
@@ -71,6 +83,7 @@ class NetworkRequestInterceptor(QWebEngineUrlRequestInterceptor):
 class MainWindow(QMainWindow):
     def __init__(self, url, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
+        self.url_scroll_positions = {}  # Initialize a dictionary to store scroll positions by URL
 
         self.initial_url = url
 
@@ -154,12 +167,15 @@ class MainWindow(QMainWindow):
 
         self.show()
 
+        self.browser.urlChanged.connect(self.reset_scroll_position)
+        self.handler = WebChannelHandler(self)  # Pass 'self' (MainWindow) to the handler
+
         # Set up QWebChannel for communication
         self.channel = QWebChannel()
         self.browser.page().setWebChannel(self.channel)
 
         # Register the 'qt' object
-        self.handler = WebChannelHandler()
+        self.handler = WebChannelHandler(self)  # Pass 'self' (MainWindow instance) here
         self.channel.registerObject('qt', self.handler)
 
         # Initialize logging attributes
@@ -168,15 +184,38 @@ class MainWindow(QMainWindow):
 
         # Set up network request interceptor
         self.interceptor = NetworkRequestInterceptor(self)
-        self.browser.page().profile().setRequestInterceptor(self.interceptor)
+        self.browser.page().profile().setUrlRequestInterceptor(self.interceptor)
 
         # Setup web channel after loading the page
-        self.browser.page().loadFinished.connect(self.setup_web_channel)
+        # self.browser.page().loadFinished.connect(self.setup_web_channel)
+
+        # Register hotkeys once
+        keyboard.add_hotkey('s', self.handle_alt_s)
+        keyboard.add_hotkey("p", self.handle_alt_p)
 
         # Start the key listener thread
         self.key_listener_thread = Thread(target=self.listen_for_q_key)
+        self.key_listener_thread.daemon = True  # Set as daemon
         self.key_listener_thread.start()
 
+    def setup_web_channel(self):
+        js = """
+        (function() {
+            var script = document.createElement('script');
+            script.src = 'qrc:///qtwebchannel/qwebchannel.js';
+            script.type = 'text/javascript';
+            script.onload = function() {
+                new QWebChannel(qt.webChannelTransport, function(channel) {
+                    window.qt = channel.objects.qt;
+                });
+            };
+            document.head.appendChild(script);
+        })();
+        // console.log("Web channel setup JavaScript injected.")
+        """
+        self.browser.page().runJavaScript(js)
+        print("Web channel setup JavaScript injected.")
+        
     def setup_frames_and_tabs(self):
         self.frame = QFrame(self)
         self.tab_widget = QTabWidget()
@@ -195,32 +234,28 @@ class MainWindow(QMainWindow):
         self.splitter.setSizes([800, 200])
         self.setCentralWidget(self.splitter)
 
-    def reset_scroll_position(self, url):
-        current_url = url.toString()
-        if current_url not in scroll_position:
-            scroll_position[current_url] = 0
+    def reset_scroll_position(self):
+        # Reset scroll position specifically for the current URL
+        current_url = self.browser.url().toString()
+        self.url_scroll_positions[current_url] = 0
+        print(f"Scroll position for {current_url} reset")
 
-    def setup_web_channel(self):
-        js = """
-        (function() {
-            var script = document.createElement('script');
-            script.src = 'qrc:///qtwebchannel/qwebchannel.js';
-            script.type = 'text/javascript';
-            script.onload = function() {
-                new QWebChannel(qt.webChannelTransport, function(channel) {
-                    window.qt = channel.objects.qt;
-                });
-            };
-            document.head.appendChild(script);
-        })();
-        """
-        self.browser.page().runJavaScript(js)
-        print("Web channel setup JavaScript injected.")
+    def update_urlbar(self, q):
+        # Ensure scroll position is managed when the URL is updated
+        url_str = q.toString()
+        if url_str not in self.url_scroll_positions:
+            self.url_scroll_positions[url_str] = 0
+        self.inject_javascript()  # Reinjection of JavaScript may be necessary
+        self.setup_web_channel()
+        self.urlbar.setText(url_str)
+        self.urlbar.setCursorPosition(0)
+        time.sleep(1)
+        self.browser.page().loadProgress.connect(self.setup_web_channel)
 
     def handle_console_message(self, level, message, line_number, source_id):
         console_message = f"Console message: {message} (Source: {source_id}, Line: {line_number})"
         if "warning" in message.lower():
-            self.console_output.append(f'<hr><spanstyle="color: yellow;">{console_message}</spanstyle=>')
+            self.console_output.append(f'<hr><span style="color: yellow;">{console_message}</span>')
         elif "error" in message.lower():
             self.console_output.append(f'<hr><span style="color: red;">{console_message}</span>')
         else:
@@ -272,9 +307,11 @@ class MainWindow(QMainWindow):
 
         file2 = ast.literal_eval('[' + file + ']')
 
-        global record_val
-        scroll_val = False
-        for i in file2:
+        global record_val,pivot
+        scroll_val = True
+        first_time_run_sleep = 0
+        for i in file2[pivot:]:
+            pivot += 1
             if not record_val:
                 if len(i) == 1:
                     key = i[0]
@@ -286,22 +323,33 @@ class MainWindow(QMainWindow):
                 if "scrolldown" in i:
                     if scroll_val is True:
                         print("time sleep 2 sec for navigation")
+                        pyautogui.moveTo(717, 395)
                         time.sleep(2)
                         pyautogui.scroll(-i[1])
                         scroll_val = False
+                        print("scroll down : ",i[1])
                     else:
                         pyautogui.scroll(-i[1])
+                        print("scroll down : ",i[1])
                 
                 if "scrollup" in i:
                     if scroll_val is True:
                         print("time sleep 2 sec for navigation")
+                        pyautogui.moveTo(717, 395)
                         time.sleep(2)
                         pyautogui.scroll(i[1])
                         scroll_val = False
+                        print("scroll up : ",i[1])
                     else:
                         pyautogui.scroll(i[1])
+                        print("scroll up : ",i[1])
 
                 if len(i) == 2:
+                    if first_time_run_sleep == 0:
+                        pyautogui.moveTo(i[0], i[1])
+                        print("time sleep first +++++++++++++++++")
+                        time.sleep(2)
+                        first_time_run_sleep += 1
                     pyautogui.moveTo(i[0], i[1])
 
                 if len(i) == 4:
@@ -317,16 +365,18 @@ class MainWindow(QMainWindow):
                         print("time sleep for 2 second")
                         time.sleep(2)
             else:
-                print("Script break", "+++++++++++++++++++++++++++++++++++++++++++++++++")
+                print("Script break")
                 break
 
     def toggle_recording(self):
-        global recording, stop_flag, scroll_position
+        global recording, stop_flag, scroll_position,play_flag
         recording = not recording
         stop_flag = not recording  # Set stop_flag to True when recording stops
         scroll_position = 0  # Reset scroll position when recording starts
         self.browser.recording = recording
         if recording:
+            play_flag = False
+            pyautogui.moveTo(717, 395)
             self.record_btn.setStyleSheet("background-color: red")
             self.record_btn.setText("Pause")
             self.start_logging()
@@ -337,23 +387,38 @@ class MainWindow(QMainWindow):
             self.record_btn.setText("Start")
             self.stop_logging()
             self.remove_javascript()
+
         print(f"Recording state: {recording}")
 
     def play(self):
-        global record_val
+        global record_val,recording,pivot,play_flag
         record_val = False
+        recording = False
+        play_flag = True
         self.browser.setUrl(QUrl(self.initial_url))
-        time.sleep(1)
+        pivot = 0
+        time.sleep(3)
         self.run_autogui_in_thread()
 
     def remove_record(self):
         open("keylogging.txt","w").close()
 
-    def update_urlbar(self, q):
-        self.setup_web_channel()
-        self.inject_javascript()
-        self.urlbar.setText(q.toString())
-        self.urlbar.setCursorPosition(0)
+    def closeEvent(self, event):
+        # Setting the stop flag for any ongoing thread loops
+        global stop_flag
+        stop_flag = True
+
+        # Request all threads to terminate
+        if self.keylogger_listener is not None:
+            self.keylogger_listener.stop()
+        if self.mouse_listener is not None:
+            self.mouse_listener.stop()
+
+        # Ensuring all components have been stopped
+        QCoreApplication.instance().quit()  # Stop the Qt event loop
+        
+        # Forcefully exit the program
+        os._exit(0)
 
     def navigate_to_url(self):
         q = QUrl(self.urlbar.text())
@@ -377,25 +442,34 @@ class MainWindow(QMainWindow):
             self.mouse_listener.stop()
 
     def keylogger(self):
+        key_log_filter = ["/","'"]
         def on_press(key):
-            global stop_flag
+            global stop_flag,recording
             if stop_flag:
                 return False  # Stop listener
             try:
-                with open('keylogging.txt', 'a') as f:
-                    f.write(f"['{key.char}'],\n")
+                if recording:
+                    with open('keylogging.txt', 'a') as f:
+                        if key.char not in key_log_filter:
+                            f.write(f"['{key.char}'],\n")
             except AttributeError:
-                with open('keylogging.txt', 'a') as f:
-                    f.write(f"['{key}'],\n")
+                if recording:
+                    with open('keylogging.txt', 'a') as f:
+                        f.write(f"['{key}'],\n")
 
         def on_release(key):
-            global stop_flag
-            if key == keyboard.Key.esc:
+            global stop_flag,recording
+            if key == pynput_keyboard.Key.esc:
+                self.record_btn.setStyleSheet("background-color: green")
+                self.record_btn.setText("Start")
+                self.stop_logging()
+                self.toggle_recording()
                 self.remove_javascript()
                 stop_flag = True
+                recording = False
                 return False
 
-        self.keylogger_listener = keyboard.Listener(on_press=on_press, on_release=on_release)
+        self.keylogger_listener = pynput_keyboard.Listener(on_press=on_press, on_release=on_release)
         self.keylogger_listener.start()
         self.keylogger_listener.join()
 
@@ -424,27 +498,36 @@ class MainWindow(QMainWindow):
 
     def record_val_loop(self):
         return True
+    
+    def handle_alt_s(self):
+        global record_val, recording, pivot
+        if not recording and not record_val and play_flag:
+            print("Alt+S pressed. Pausing playback.")
+            record_val = self.record_val_loop()
+            # print(pivot)
+
+    def handle_alt_p(self):
+        global record_val, recording, pivot
+        if not recording and record_val and play_flag:
+            print("Alt+P pressed. Resuming playback.")
+            record_val = False
+            pivot = pivot-1
+            self.run_autogui_in_thread()
 
     def listen_for_q_key(self):
         def on_press(key):
-            if key == keyboard.KeyCode.from_char('s'):
-                global record_val
-                print("Q key pressed. Closing application.")
-                record_val = self.record_val_loop()
-                print(record_val,"++++++++++++++++++++")
-
-            if key == keyboard.KeyCode.from_char("q"):
+            if key == pynput_keyboard.KeyCode.from_char("q"):
                 self.close()
-                
-        with keyboard.Listener(on_press=on_press) as listener:
+            
+        with pynput_keyboard.Listener(on_press=on_press) as listener:
             listener.join()
 
 # Creating a PyQt5 application
 app = QApplication(sys.argv)
-app.setApplicationName("Geek Browser")
+app.setApplicationName("Testing Tool")
 
 # Creating a main window object
-url = "https://en.wikipedia.org/wiki/Empire"
+url = input("Enter URL : ")
 window = MainWindow(url)
 
 # Start the application loop
